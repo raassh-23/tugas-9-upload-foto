32c19bc8e998fc25bc13c2d2dc47ad62af2dbe3b
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-11-30 22:47:15 +0700

    change appearance

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 
 // Untuk koneksi database local
-// $server = "localhost";
-// $user = "root";
-// $password = "";
-// $nama_database = "pendaftaran_siswa";
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "pendaftaran_siswa";
 
 // Untuk koneksi database heroku
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $cleardb_url["host"];
-$user = $cleardb_url["user"];
-$password = $cleardb_url["pass"];
-$nama_database = substr($cleardb_url["path"],1);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $server = $cleardb_url["host"];
+// $user = $cleardb_url["user"];
+// $password = $cleardb_url["pass"];
+// $nama_database = substr($cleardb_url["path"],1);
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
--- list-siswa.php
+++ list-siswa.php
@@ -2,63 +2,73 @@
 
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
 
-<body>
-    <header>
-        <h3>Siswa yang sudah mendaftar</h3>
-    </header>
+<body class="bg-light text-dark d-flex flex-column">
+    <div class="container-fluid content flex-grow-1 d-flex flex-column justify-content-center">
+        <header class="text-center mt-4">
+            <h3>Siswa yang sudah mendaftar</h3>
+        </header>
 
-    <nav>
-        <ul>
-            <li><a href="index.php">Kembali</a></li>
-            <li><a href="form-daftar.php">[+] Tambah Baru</a></li>
-        </ul>
-    </nav>
+        <section class="mb-4 d-flex flex-column align-items-center justify-content-center">
+            <article class="col-md-10">
+                <table class="table table-striped table-hover mt-2">
+                    <thead>
+                        <tr class="table-dark">
+                            <th scope="col">No</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">Jenis Kelamin</th>
+                            <th scope="col">Agama</th>
+                            <th scope="col">Sekolah Asal</th>
+                            <th scope="col">Tindakan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = "SELECT * FROM calon_siswa";
+                        $query = mysqli_query($db, $sql);
 
-    <table border="1">
-    <thead>
-        <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>Jenis Kelamin</th>
-            <th>Agama</th>
-            <th>Sekolah Asal</th>
-            <th>Tindakan</th>
-        </tr>
-    </thead>
-    <tbody>
+                        while ($siswa = mysqli_fetch_array($query)) {
+                            echo "<tr>";
 
-        <?php
-        $sql = "SELECT * FROM calon_siswa";
-        $query = mysqli_query($db, $sql);
+                            echo "<td>" . $siswa['id'] . "</td>";
+                            echo "<td>" . $siswa['nama'] . "</td>";
+                            echo "<td>" . $siswa['alamat'] . "</td>";
+                            echo "<td>" . $siswa['jenis_kelamin'] . "</td>";
+                            echo "<td>" . $siswa['agama'] . "</td>";
+                            echo "<td>" . $siswa['sekolah_asal'] . "</td>";
 
-        while($siswa = mysqli_fetch_array($query)){
-            echo "<tr>";
+                            echo "<td>";
+                            echo "<a class=\"btn btn-sm btn-warning\" href='form-edit.php?id=" . $siswa['id'] . "'>Edit</a> ";
+                            echo "<a class=\"btn btn-sm btn-danger\" href='hapus.php?id=" . $siswa['id'] . "'>Hapus</a>";
+                            echo "</td>";
 
-            echo "<td>".$siswa['id']."</td>";
-            echo "<td>".$siswa['nama']."</td>";
-            echo "<td>".$siswa['alamat']."</td>";
-            echo "<td>".$siswa['jenis_kelamin']."</td>";
-            echo "<td>".$siswa['agama']."</td>";
-            echo "<td>".$siswa['sekolah_asal']."</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <p>Total: <?php echo mysqli_num_rows($query) ?></p>
+            </article>
 
-            echo "<td>";
-            echo "<a href='form-edit.php?id=".$siswa['id']."'>Edit</a> | ";
-            echo "<a href='hapus.php?id=".$siswa['id']."'>Hapus</a>";
-            echo "</td>";
+            <article class="col-md-10">
+                <a class="btn btn-primary" href="form-daftar.php">[+] Tambah Baru</a>
+                <a class="btn btn-primary" href="index.php">Kembali</a>
+            </article>
+        </section>
 
-            echo "</tr>";
-        }
-        ?>
+    </div>
 
-    </tbody>
-    </table>
+</body>
 
-    <p>Total: <?php echo mysqli_num_rows($query) ?></p>
-
-    </body>
 </html>
