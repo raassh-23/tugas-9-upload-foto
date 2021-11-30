0b19b243a1183f029ce4fe5a36e626f27a7610e1
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-11-30 15:16:13 +0700

    Add connection to cleardb

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$password = "";
-$nama_database = "pendaftaran_siswa";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $cleardb_url["host"];
+$user = $cleardb_url["user"];
+$password = $cleardb_url["pass"];
+$nama_database = substr($cleardb_url["path"],1);
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
